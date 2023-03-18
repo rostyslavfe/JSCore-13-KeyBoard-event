e07bd9b9639aaacb07b53116ddf1012ec2ce4c7c
rostyslav_vlasov <111677844+rostyslavfe@users.noreply.github.com>
2023-03-18 12:59:45 +0200

    second task

---
+++ script2.js
@@ -0,0 +1,40 @@
+let all = document.querySelectorAll('div');
+let space = document.querySelector('.lower');
+let shiftLeft = document.getElementById('left');
+let shiftRight = document.getElementById('right');
+
+function keydoard(){
+    window.addEventListener('keydown', function(event){
+        //loop
+        for(let i=0; i<all.length; i++){
+            if(all[i].innerText.toLowerCase() == event.key.toLowerCase()){
+                all[i].style.backgroundColor = 'gray'
+            }
+        }
+        //key "Space"
+        if(event.keyCode === 32){
+            space.style.backgroundColor = 'gray'
+            window.addEventListener('keyup', function(){
+                space.style.backgroundColor = 'white'
+            })
+        }
+        //keys "Shifts"
+        if(event.shiftKey === true && event.location === 1){
+            shiftLeft.style.backgroundColor = 'gray'
+            shiftRight.style.backgroundColor = 'white'
+        }
+        if(event.shiftKey === true && event.location === 2){
+            shiftLeft.style.backgroundColor = 'white'
+            shiftRight.style.backgroundColor = 'gray'
+        }
+    });
+    window.addEventListener('keyup', function(event){
+        for(let i=0; i<all.length; i++){
+            if(all[i].innerText.toLowerCase() == event.key.toLowerCase()){
+                all[i].style.backgroundColor = 'white'
+            }
+        }
+    });
+}
+keydoard()
+
